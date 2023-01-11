07b50bf3e692c817abb32e2c00580b410fd93708
cuishuyuan <cuisy0703@163.com>
2023-01-12 00:56:05 +0800

    Initial commit

---
+++ src/main/java/com/course/booking/Demo.java
@@ -0,0 +1,17 @@
+package com.course.booking;
+
+
+public class Demo {
+
+    private String filed;
+
+    public String getFiled() {
+        return filed;
+    }
+
+    public void setFiled(String filed) {
+        this.filed = filed;
+    }
+}
+
+
